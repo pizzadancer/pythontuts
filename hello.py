168e53ac708167b6a6a39eec65f6ca4657d1f89d
pizzadancer <>
2019-09-30 12:11:53 -0400

    testing

---
+++ hello.py
@@ -0,0 +1,2 @@
+def hello(first_name, last_name):
+    print("Hello, {} {}".format(first_name, last_name))
---
+++ stringstuff.py
@@ -0,0 +1,6 @@
+from hello import hello
+first_name = "john"
+last_name = "adams"
+
+
+hello(first_name, last_name)
